2ecfc06d2fca6837ad759974cb5bf7523634fe01
Miguel ACH <ayalachavezmiguel@gmail.com>
2023-06-19 21:52:54 -0400

    Fase 1

---
+++ FrontEnd/src/Componentes/Testimonio.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../HojasDeEstilo/Testimonio.css";
+const Testimonio = ({ foto, nombre, pais, cargo, empresa, testimonio }) => {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={foto}
+        alt={`Fotografia de ${nombre}`}
+      />
+      <div className="contenedor-info">
+        <p className="nombre-testimonio">
+          <strong>{nombre}</strong> in <strong>{pais}</strong>
+        </p>
+        <p className="cargo-testimonio">
+          <strong>{cargo}</strong> at <strong>{empresa}</strong>
+        </p>
+        <p className="texto-testimonio">"{testimonio}"</p>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonio;
